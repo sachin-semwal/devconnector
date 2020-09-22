4f0d5103cf088c37efd83c6fd6d0ef33aedcb63b
sachin semwal <semwalsachin@ymail.com>
2020-09-22 20:08:14 +0530

    second draft

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,13 +5,13 @@ import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
 import CommentForm from "../post/CommentForm";
 import CommentItem from "../post/CommentItem";
-import { getPost, getPosts } from "../../actions/post";
+import { getPost } from "../../actions/post";
 import { Link } from "react-router-dom";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
-  }, [getPost]);
+  }, [getPost, match.params.id]);
   return loading || post === null ? (
     <Spinner />
   ) : (
